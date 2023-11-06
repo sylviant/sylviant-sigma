e74f4be084bd5d2764627eb47de61c53868d8667
sylviant <146768715+sylviant@users.noreply.github.com>
2023-11-06 05:26:54 +0100

    Add files via upload

---
+++ javascrip.js
@@ -0,0 +1,29 @@
+const loader=document.querySelector('.loader');
+window.addEventListener('load', () => {
+
+    loader.classList.add('fondu-out');
+})
+
+/*const partie = document.getElementById("partie");
+
+        partie.addEventListener("mouseenter", () => {
+            partie.style.transform = "translate(10px, 10px)"; // Déplace de 10 pixels vers la droite et le bas
+        });
+
+        partie.addEventListener("mouseleave", () => {
+            partie.style.transform = "translate(0, 0)"; // Réinitialise la position
+        });
+
+        const motClic = document.getElementById("mot-clic");
+        const optionsList = document.getElementById("options-list");
+
+        motClic.addEventListener("click", function () {
+            // Affiche ou masque les options en fonction de leur état actuel
+            if (optionsList.style.display === "none") {
+                optionsList.style.display = "block";
+            } else {
+                optionsList.style.display = "none";
+            }
+        });*/
+
+     
